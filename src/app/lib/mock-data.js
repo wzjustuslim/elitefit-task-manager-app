ffd4480d728b469a30c05d92685f9bfa1a531b8f
Justus Lim <70901244+wzjustuslim@users.noreply.github.com>
2024-04-01 21:59:11 +0800

    Add mock data

---
+++ src/app/lib/mock-data.js
@@ -0,0 +1,27 @@
+const tasks = [
+  {
+    title: "Create a new task manager app",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    due_date: "2023-06-14",
+    priority: "high",
+    status: "incomplete",
+  },
+  {
+    title: "Create a meme scrapper app",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    due_date: "2024-07-04",
+    priority: "medium",
+    status: "incomplete",
+  },
+  {
+    title: "Start ruby on rails project",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    due_date: "2024-09-12",
+    priority: "low",
+    status: "complete",
+  },
+];
+
+module.exports = {
+  tasks,
+};
